f2b2c437e02510a8de984163cede362bf62ade26
EduardoMotta2509 <eduardo.motta@estudiante.ucsm.edu.pe>
2025-04-28 09:07:10 +0600

    hola

---
+++ Fase II/Práctica Nº 05/Propuestos/Ejercicio 1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#include "Producto.h"
+
+int main(){
+    p1.setNombre("Casaca").setPrecio(149.90).setDescuento(0.5);
+    p2.setNombre("Jean").setPrecio(89.90).setDescuento(0.25);
+    p3.setNombre("Polo").setPrecio(39.90).setDescuento(0.15);
+    // Declarar un arreglo estático de 3 objetos Producto
+    float preciosFinales[3] = {
+        p1.calcularPrecioFinal(),
+        p2.calcularPrecioFinal(),
+        p3.calcularPrecioFinal()
+    };
+
+    // Recorrer el arreglo y sumar los precios finales
+    for (int i = 0; i < 3; i++) {
+        float totalAcumulado = totalAcumulado + preciosFinales[i];
+    }
+    cout<<"El total acumulado es: "<<totalAcumulado;
+    return 0;
+}
