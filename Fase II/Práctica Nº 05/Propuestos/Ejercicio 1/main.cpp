d69ff42571f7d5117fd878566f7895f5687fb908
EduardoMotta2509 <eduardo.motta@estudiante.ucsm.edu.pe>
2025-04-28 09:18:10 +0600

    asd

--- Fase II/Práctica Nº 05/Propuestos/Ejercicio 1/main.cpp
+++ Fase II/Práctica Nº 05/Propuestos/Ejercicio 1/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 #include "Producto.h"
 
 int main(){
+    Producto p1;
+    Producto p2;
+    Producto p3;
+    
     p1.setNombre("Casaca").setPrecio(149.90).setDescuento(0.5);
     p2.setNombre("Jean").setPrecio(89.90).setDescuento(0.25);
     p3.setNombre("Polo").setPrecio(39.90).setDescuento(0.15);
@@ -13,10 +17,10 @@ int main(){
         p2.calcularPrecioFinal(),
         p3.calcularPrecioFinal()
     };
-
+    float totalAcumulado;
     // Recorrer el arreglo y sumar los precios finales
     for (int i = 0; i < 3; i++) {
-        float totalAcumulado = totalAcumulado + preciosFinales[i];
+        totalAcumulado = totalAcumulado + preciosFinales[i];
     }
     cout<<"El total acumulado es: "<<totalAcumulado;
     return 0;
